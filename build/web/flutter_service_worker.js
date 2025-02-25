09ba6feeee6cf2900f872fcaa10f6a6482afc9a7
Your Name <your-email@example.com>
2025-02-26 02:04:47 +0900

    Update package.json and vercel.json for Flutter web deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "73d95c1830dc03ac3ac41c90c36fe393
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "70b5da6909a254c8112313d99aa40de7",
+"flutter_bootstrap.js": "8c82201202d814ef165183dd92fe2788",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
